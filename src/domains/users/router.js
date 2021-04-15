797f52e2550a9ad82efb4a7d915c49040fd4018b
minhchau <kinglatyoftheworld@gmail.com>
2021-04-15 23:02:31 +0700

    udpate ui

--- src/domains/users/router.js
+++ src/domains/users/router.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const create_one = require('./controllers/create_one')
 const get_one = require('./controllers/get_one')
 const update_one = require('./controllers/update_one')
 
 const validate = require('../../middleware/check_validate')
 const dataExample = require('./models/validate')
 
-
-router.post('/', validate(dataExample.create), create_one)
 
 router.get('/:id', get_one)
 
