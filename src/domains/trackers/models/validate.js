71fa6fd6635b945df8005c338b1e353bcecc6a88
minhchau <kinglatyoftheworld@gmail.com>
2021-04-16 18:16:23 +0700

    update schedule

--- src/domains/trackers/models/validate.js
+++ src/domains/trackers/models/validate.js
@@ -2,19 +2,17 @@ const create = {
     additionalProperties: false,
     type: 'object',
     properties: {
-        user_id: { type: 'string' },
         time: { type: 'string' },
         step: { type: 'integer' },
         weight: { type: 'integer' },
     },
-    required: ['user_id', 'time', 'step', 'weight']
+    required: ['time', 'step', 'weight']
 }
 
 const update = {
     additionalProperties: false,
     type: 'object',
     properties: {
-        user_id: { type: 'string' },
         time: { type: 'string' },
         step: { type: 'integer' },
         weight: { type: 'integer' },
